0ea4a06df7d516aa0bee7f1929db62f17b495204
Matthew Henderson <Matthew.Henderson@microsoft.com>
2018-03-03 19:26:35 -0800

    fixing local setup for load test

--- provision.ps1
+++ provision.ps1
@@ -4,4 +4,6 @@ az group create -n brk2050 -l eastus
 az group deployment create -g brk1155 -n brk1155-setup --template-file .\brk1155-template.json
 az group deployment create -g brk2050 -n brk2050-setup --template-file .\brk2050-template.json
 
-az group deployment show -g brk2050 -n brk2050-setup --query properties.outputs.ehcxnstr.value
+az group deployment show -g brk2050 -n brk2050-setup --query properties.outputs.ehcxnstr.value > $env:TEMP\brk2050-scratch.txt
+az group deployment show -g brk2050 -n brk2050-setup --query properties.outputs.ehname.value >> $env:TEMP\brk2050-scratch.txt
+notepad.exe $env:TEMP\brk2050-scratch.txt
